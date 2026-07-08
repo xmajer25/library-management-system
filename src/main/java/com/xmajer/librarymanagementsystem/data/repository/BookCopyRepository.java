4101ebfe8c1e6309ef94b71b0e800cf76a2161df
Jakub Majer <567782@mail.muni.cz>
2026-07-08 18:39:42 +0200

    fix: change JpaRepository from UUID to Long id

--- src/main/java/com/xmajer/librarymanagementsystem/data/repository/BookCopyRepository.java
+++ src/main/java/com/xmajer/librarymanagementsystem/data/repository/BookCopyRepository.java
@@ -3,7 +3,5 @@ package com.xmajer.librarymanagementsystem.data.repository;
 import com.xmajer.librarymanagementsystem.data.model.BookCopy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface BookCopyRepository extends JpaRepository<BookCopy, UUID> {
+public interface BookCopyRepository extends JpaRepository<BookCopy, Long> {
 }
--- src/main/java/com/xmajer/librarymanagementsystem/data/repository/BookRepository.java
+++ src/main/java/com/xmajer/librarymanagementsystem/data/repository/BookRepository.java
@@ -3,7 +3,5 @@ package com.xmajer.librarymanagementsystem.data.repository;
 import com.xmajer.librarymanagementsystem.data.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface BookRepository extends JpaRepository<Book, UUID> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
