929dcc25a2b09a1d4fafa219992d569a0a51472f
Jakub Majer <567782@mail.muni.cz>
2026-07-09 20:58:04 +0200

    test: test containers setup and annotations

--- src/test/java/com/xmajer/librarymanagementsystem/integration/BaseIntegrationTest.java
+++ src/test/java/com/xmajer/librarymanagementsystem/integration/BaseIntegrationTest.java
@@ -1,4 +1,23 @@
 package com.xmajer.librarymanagementsystem.integration;
 
-public class BaseIntegrationTest {
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class BaseIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    protected static final PostgreSQLContainer POSTGRES =
+            new PostgreSQLContainer("postgres:16-alpine")
+                    .withDatabaseName("library_test_db")
+                    .withUsername("test_user")
+                    .withPassword("test_password");
 }
--- src/test/java/com/xmajer/librarymanagementsystem/integration/api/BookApiIntegrationTest.java
+++ src/test/java/com/xmajer/librarymanagementsystem/integration/api/BookApiIntegrationTest.java
@@ -1,6 +1,12 @@
 package com.xmajer.librarymanagementsystem.integration.api;
 
 import com.xmajer.librarymanagementsystem.integration.BaseIntegrationTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 
+@AutoConfigureMockMvc
 public class BookApiIntegrationTest extends BaseIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
 }
