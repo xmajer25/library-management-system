e5f970802685c18f256896861d3b19e4060dbdb0
Jakub Majer <567782@mail.muni.cz>
2026-07-11 14:08:30 +0200

    refactor: optimize imports

--- src/main/java/com/xmajer/librarymanagementsystem/controller/BookCopyController.java
+++ src/main/java/com/xmajer/librarymanagementsystem/controller/BookCopyController.java
@@ -11,7 +11,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
